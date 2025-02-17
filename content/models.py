06dce97be642fa522df0d3f40531a73e7e448775
Randevit <parus.remote@yandex.ru>
2025-02-17 16:20:01 +0200

    add content

--- content/models.py
+++ content/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from .validators import validate_year
 
 from .constant import (
     GENRE_NAME_MAX_LENGTH,
@@ -10,40 +11,94 @@ from .constant import (
 )
 
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(
         'Наименование',
         max_length=CATEGORY_NAME_MAX_LENGTH
     )
     slug = models.SlugField(
         'Код',
-        max_length=CATEGORY_SLUG_MAX_LENGTH
+        max_length=CATEGORY_SLUG_MAX_LENGTH,
+        unique=True
     )
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
 
-class Genres(models.Model):
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
     name = models.CharField(
         'Наименование',
         max_length=GENRE_NAME_MAX_LENGTH
     )
     slug = models.SlugField(
         'Код',
-        max_length=GENRE_SLUG_MAX_LENGTH
+        max_length=GENRE_SLUG_MAX_LENGTH,
+        unique=True
     )
 
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ('name',)
 
-class Titles(models.Model):
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
     name = models.CharField(
         'Наименование',
         max_length=TITLE_NAME_MAX_LENGTH
     )
     year = models.PositiveSmallIntegerField(
         'Год выхода',
-        min=1900,
-        max=2025
+        validators=[validate_year]
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        through='GenreTitle'
     )
     category = models.ForeignKey(
-        Categories,
+        Category,
         verbose_name='Категория',
         on_delete=models.CASCADE
     )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        blank=True, null=True,
+        on_delete=models.SET_NULL)
+    genre = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр',
+        blank=True, null=True,
+        on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = 'Произведение и жанр'
+        verbose_name_plural = 'Произведения и жанры'
+
+    def __str__(self):
+        return f'{self.title}, жанр <-> {self.genre}'
---
+++ content/validators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+from .constant import FIRST_YEAR_OF_MOVIES
+
+
+def validate_year(value):
+    if value > timezone.now().year:
+        raise ValidationError(
+            f'Год {value} из будующего!',
+            params={'value': value},
+        )
+    if value < FIRST_YEAR_OF_MOVIES:
+        raise ValidationError(
+            f'Год {value} меньше первого года кинематографа!',
+            params={'value': value},
+        )

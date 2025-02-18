1a77e4e25b56b20bc87ee3cdee7f460650eb380d
SokolovG <griso2010@yandex.ru>
2025-02-18 13:03:37 +0200

    Structure updated.

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import User
-
-@admin.register(User)
-class ModelNameAdmin(admin.ModelAdmin):
-    list_display = ('email', 'role', 'username')
--- users/constants.py
+++
@@ -1,6 +0,0 @@
-MAX_EMAIL_LENGTH = 254
-MAX_ROLE_LENGTH = 20
-MAX_CONFIRMATION_CODE_LENGTH = 6
-
-EMAIL_SUBJECT = 'Confirm account'
-EMAIL_MESSAGE = 'Please confirm your account with code: '

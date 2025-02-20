0cf853733afde6512e79ca4944e08581adee8974
Randevit <parus.remote@yandex.ru>
2025-02-20 12:47:43 +0200

    fix urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -37,7 +37,7 @@ router_v1.register(
 
 
 urlpatterns = [
-    path('api/v1/', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/', SignUpView.as_view()),
-    path('v1/auth/token/',TokenObtainView .as_view()),
+    path('v1/auth/token/', TokenObtainView .as_view()),
 ]

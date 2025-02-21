07d2a201810156540fdb5d53db58dd7f8fb8a2b3
SokolovG <griso2010@yandex.ru>
2025-02-21 10:20:40 +0200

    Changed exceptions structure

--- api_yamdb/users/exceptions/api.py
+++ api_yamdb/users/exceptions/api.py
@@ -1,8 +1,6 @@
 from rest_framework.exceptions import APIException
 from rest_framework import status
 
-from rest_framework import serializers
-
 
 class UserNotFoundException(APIException):
     status_code = status.HTTP_404_NOT_FOUND

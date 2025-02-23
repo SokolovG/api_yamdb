af2dc4fd19298e2aa2da515cd6f19859e6af66ab
khalikov-sa <khalikov.ss@yandex.ru>
2025-02-23 09:50:30 +0300

    review_fix

--- api_yamdb/api/reviews/views.py
+++ api_yamdb/api/reviews/views.py
@@ -1,4 +1,3 @@
-"""Вьюсеты для работы с отзывами и комментариями."""
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
@@ -21,16 +20,6 @@ class ReviewViewSet(viewsets.ModelViewSet):
         """Получение произведения по ID."""
         return get_object_or_404(Title, id=self.kwargs.get("title_id"))
 
-    def update_rating(self, title) -> None:
-        """Обновление рейтинга произведения."""
-        reviews = title.reviews.all()
-        if reviews.exists():
-            total_score = sum(review.score for review in reviews)
-            title.rating = round(total_score / reviews.count(), 2)
-        else:
-            title.rating = None
-        title.save()
-
     def get_queryset(self) -> models.QuerySet[Review]:
         """Возвращает все отзывы произведения."""
         return self.get_title().reviews.all()
@@ -38,27 +27,22 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer) -> None:
         """Переопределение метода добавления отзыва.
 
-        Проверка уникальности. Обновлением рейтинга.
+        Проверка уникальности.
         """
-        if Review.objects.filter(
-            title=self.get_title(), author=self.request.user
-        ).exists():
+        title = self.get_title()
+        if title.reviews.filter(author=self.request.user).exists():
             raise ValidationError(
                 "Вы уже оставляли отзыв на это произведение."
             )
-        serializer.save(author=self.request.user, title=self.get_title())
-        self.update_rating(title=self.get_title())
+        serializer.save(author=self.request.user, title=title)
 
     def perform_update(self, serializer) -> None:
-        """Переопределение метода обновления отзыва с обновлением рейтинга."""
-        instance = serializer.save()
-        self.update_rating(instance.title)
+        """Переопределение метода обновления отзыва."""
+        serializer.save()
 
     def perform_destroy(self, instance) -> None:
-        """Переопределение метода удаления отзыва с обновлением рейтинга."""
-        title = instance.title
+        """Переопределение метода удаления отзыва."""
         instance.delete()
-        self.update_rating(title)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -74,7 +58,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self) -> models.QuerySet[Comment]:
         """Возвращает все комментарии для отзыва."""
-        return self.get_review().comments.all()
+        return self.get_review().comments.all().order_by('-pub_date')
 
     def perform_create(self, serializer) -> None:
         """Переопределение метода добавления комментария."""
